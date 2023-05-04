c95e5d3ce2d4e1f690f124b53fb2258f6876b783
Katie L Nuchols <knuchol1@hydra1.eecs.utk.edu>
2023-05-04 11:30:00 -0400

    changing email name

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-let val;
-function GetName(){
-    const val = document.getElementById('Email').value;
-    return val;
-}
-function SetName(){
-    let val = GetName();
-    document.getElementById('EmailSet').value= val2;
-}
-(function(console){
-console.save = function(data, filename){
-
-    if(!data) {
-        console.error('Console.save: No data')
-        return;
-    }
-
-    if(!filename) filename = 'console.json'
-
-    if(typeof data === "object"){
-        data = JSON.stringify(data, undefined, 4)
-    }
-
-    var blob = new Blob([data], {type: 'text/json'}),
-        e    = document.createEvent('MouseEvents'),
-        a    = document.createElement('a')
-
-    a.download = filename
-    a.href = window.URL.createObjectURL(blob)
-    a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
-    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
-    a.dispatchEvent(e)
- }
-})(console)
+var person = {
+    account: []
+};
+person.account.push({email: 'Example@email.com', password: '12345678' })
+var json = JSON.stringify(person);
+var fs = require('fs');
+fs.writeFile('jsonfile.json', json, 'utf8', callback);
