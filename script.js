cc63d959f916d392ce97a49bc4ff6f1e9d71e76d
Katie L Nuchols <knuchol1@hydra1.eecs.utk.edu>
2023-05-04 23:05:35 -0400

    im not sure

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-var person = {
-    account: []
-};
-person.account.push({email: 'Example@email.com', password: '12345678' })
-var json = JSON.stringify(person);
-var fs = require('fs');
-function WriteTO(){
-    fs.writeFile('jsonfile.json', json, 'utf8', callback);
+// Retrieve existing data from local storage
+var existingData = localStorage.getItem('myData');
+
+// Parse existing data from CSV or JSON format
+var data = existingData ? JSON.parse(existingData) : [];
+
+// Add new data to array
+data.push({ name: 'John', email: 'john@example.com' });
+
+// Convert array to CSV or JSON format
+var csvData = 'Name,Email\n';
+for (var i = 0; i < data.length; i++) {
+  csvData += data[i].name + ',' + data[i].email + '\n';
 }
+// OR
+var jsonData = JSON.stringify(data);
+
+// Save updated data to local storage
+localStorage.setItem('myData', jsonData);
+
+// Create file blob
+var blob = new Blob([csvData || jsonData], { type: 'text/csv' || 'application/json' });
+
+// Create download link
+var url = URL.createObjectURL(blob);
+var link = document.createElement('a');
+link.setAttribute('href', url);
+link.setAttribute('download', 'myData.csv' || 'myData.json');
+
+// Click download link to initiate download
+document.body.appendChild(link);
+link.click();
+document.body.removeChild(link);
